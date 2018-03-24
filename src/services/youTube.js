0fb7c02ec7ffeb29a02f4674bc2067788218f39a
jennqiao <jennifercqiao@gmail.com>
2018-03-24 11:40:38 -0700

    updated spacing

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,29 +1,27 @@
 angular.module('video-player')
-.service('youTube', function($http){
-  this.search =  function(query, callback) {
-    $http ({
-    method: 'GET',
-    url: 'https://www.googleapis.com/youtube/v3/search',
-    params: {
-      embeddable: true,
-      type: 'video',
-      q: query,
-      maxResults:10,
-      part: 'snippet',
-      key: 'AIzaSyDWmoDnEgQLI4_tO1-juGPIkIvmg9dHaXQ'
-    }
-  }).then(function successCallback(response) {
-      // this callback will be called asynchronously
-      // when the response is available
-      callback(response.data.items);
-      //console.log("success", response);
-      //console.log("videos", videos);
-      //sconsole.log("context", context);
-    }, function errorCallback(response) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.   
-       console.log("error", response);
+  .service('youTube', function($http){
+    this.search =  function(query, callback) {
+      $http ({
+        method: 'GET',
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        params: {
+          embeddable: true,
+          type: 'video',
+          q: query,
+          maxResults:10,
+          part: 'snippet',
+          key: 'AIzaSyDWmoDnEgQLI4_tO1-juGPIkIvmg9dHaXQ'
+        }
+      }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+        callback(response.data.items);
+    
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.   
+        console.log("error", response);
 
-    });
-  }
-});
+      });
+    };
+  });
